a9ce22537c9330c29a69294b8f56252e6a8ba0f4
guillaume <gpitot@nine.com.au>
2019-11-03 16:43:33 +1100

    comments

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./sass/index.scss";
 
 import SmoothScrollingArticle, { Item } from "smooth-scroll";
 
+//maps a number x from values [a,b] to range [c,d]
 const map = (x, a, b, c, d) => ((x - a) * (d - c)) / (b - a) + c;
 
 const images = document.querySelectorAll(".content__item-img");
@@ -12,10 +13,12 @@ const items = [];
 images.forEach(element => {
   const item = new Item({ element });
   item.add({
-    ease: 0.1,
-    minChange: 0.05,
+    ease: 0.1, //ease is how smooth you want the animation to move lower is smoother, but may cause performance issues.
     render: (image, current) => {
       //current will be 0-1
+      //render receives 2 params, the element, and the current position on page from bottom to top of screen
+      //bottom is 0
+      //top is 1
       const scale = map(current, 0, 1, 1, 1.5);
       image.style.transform = `scale3d(${scale},${scale},1)`;
     }
@@ -30,7 +33,6 @@ titles.forEach(element => {
   const item = new Item({ element });
   item.add({
     ease: 0.1,
-    minChange: 0.05,
     render: (title, current) => {
       const val = map(current, 0, 1, 50, -50);
       title.style.transform = `translate3d(0,${val}px,0)`;
